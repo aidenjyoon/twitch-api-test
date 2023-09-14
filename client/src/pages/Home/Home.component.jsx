f5874bb13c046954c6b53240a2fe8177fb7208fb
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-09-13 19:42:27 -0700

    fixed duplicated navbar

--- client/src/pages/Home/Home.component.jsx
+++ client/src/pages/Home/Home.component.jsx
@@ -1,9 +1,6 @@
-import NavBar from "../../components/NavBar/NavBar.component";
-
 const Home = () => {
   return (
     <>
-      <NavBar />
       <h1>Home Page</h1>
       <p>asdfasdfa</p>
     </>
