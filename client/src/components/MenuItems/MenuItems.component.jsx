e42c1d1e171ae179c3f2892869bf0374102003dd
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-09-16 21:50:22 -0700

    setup MenuItems Component

---
+++ client/src/components/MenuItems/MenuItems.component.jsx
@@ -0,0 +1,18 @@
+import { MenuContainer } from "./MenuItems.styles";
+
+const MenuItems = () => {
+  return (
+    <>
+      <MenuContainer>
+        <ul>
+          <li>Search</li>
+          <li>Channels</li>
+          <li>Games</li>
+          <li>Subscribers</li>
+        </ul>
+      </MenuContainer>
+    </>
+  );
+};
+
+export default MenuItems;
---
+++ client/src/components/MenuItems/MenuItems.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MenuContainer = styled.div`
+  background: red;
+
+  height: 200px;
+  width: 100px;
+`;
